e14f0ad2327dc5effd64dd206617c2014da1ab8a
Jonathan Morton <djlarrikin@gmail.com>
2014-07-18 22:53:17 +0900

    Initial commit under Android Studio, chapter 12 finished

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,92 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.UUID;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+	public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+	
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
+	
+	public static CrimeFragment newInstance(UUID crimeId){
+		Bundle args = new Bundle();
+		args.putSerializable(EXTRA_CRIME_ID, crimeId);
+		CrimeFragment fragment = new CrimeFragment();
+		fragment.setArguments(args);
+		
+		return fragment;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
+		mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		mTitleField = (EditText)v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence c, int start, int before, int count) {
+				mCrime.setTitle(c.toString());
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence c, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		mDateButton = (Button)v.findViewById(R.id.crime_date);
+		SimpleDateFormat format = new SimpleDateFormat("h:m a EEEE MMM dd, yyyy", Locale.getDefault());
+		String humanDate = format.format(mCrime.getDate());
+		mDateButton.setText(humanDate);
+		mDateButton.setEnabled(false);
+		
+		mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				mCrime.setSolved(isChecked);
+				
+			}
+		});
+		
+		return v;
+	}
+
+}
