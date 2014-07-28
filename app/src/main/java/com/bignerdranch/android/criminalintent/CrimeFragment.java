58842519dc26bdfdfa1bad0ee235e066ded55432
djlarrikin <djlarrikin@gmail.com>
2014-07-28 15:19:42 +0900

    Continuing chapter 12

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -80,7 +80,7 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getActivity().getSupportFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog = new DatePickerFragment().newInstance(mCrime.getDate());
                 dialog.show(fm, DIALOG_DATE);
             }
         });
